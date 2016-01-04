acc9ad096eeb8ceb3aec5e18ac4b002ca9f87255
TonyHsu <tonyc.t.hsu@gmail.com>
2016-01-04 21:54:59 +0800

    user register through devise, user can edit profile after registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -48,7 +50,7 @@ Rails.application.routes.draw do
   #   end
   #   resources :posts, concerns: :toggleable
   #   resources :photos, concerns: :toggleable
-root to: "home#index"
+root to: "users#index" 
   # Example resource route within a namespace:
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
