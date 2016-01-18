2a39118854f054754d4dec20322ef4b0c46b5d0b
TonyHsu <tonyc.t.hsu@gmail.com>
2016-01-18 18:59:56 +0800

    login logout registration api

--- app/controllers/api_v1/users_controller.rb
+++ app/controllers/api_v1/users_controller.rb
@@ -1,4 +1,5 @@
 class ApiV1::UsersController < ApiController
+	
 
 
   def index
@@ -8,7 +9,25 @@ class ApiV1::UsersController < ApiController
 	def show
   	@user = User.find(params[:id])
   	@orders = @user.orders
-  
+ 	end
+
+ 	def create
+ 		x=User.new(reg_params)
+		if x.save
+      render :json => { :message => "Registration success!",
+                        :auth_token => x.authentication_token,
+                        :user_id => x.id}
+    else 
+    	# validates :email, :uniqueness =>{ :message => "Email has already been registered!" },:status => 401
+      render :json => { :messages => "#{x.errors.full_messages}" }, :status => 401
+    end
+ 	end
+
+protected
+	
+	def reg_params
+		params.permit(:email, :password, :password_confirmation)
 	end
+
 
 end
